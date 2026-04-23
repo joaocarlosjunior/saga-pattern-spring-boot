59844de12b47f239a371f889344e1ddfc0d5137c
João Carlos Júnior <joaocarlos.cic1@gmail.com>
2026-04-23 19:54:52 -0300

    feat: adiciona envio de evento para topico de order

--- orders-service/src/main/java/com/joaocarlos/orders/service/OrderServiceImpl.java
+++ orders-service/src/main/java/com/joaocarlos/orders/service/OrderServiceImpl.java
@@ -4,14 +4,23 @@ import com.joaocarlos.core.dto.Order;
 import com.joaocarlos.core.types.OrderStatus;
 import com.joaocarlos.orders.dao.jpa.entity.OrderEntity;
 import com.joaocarlos.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import com.joaocarlos.core.dto.events.OrderCreatedEvent;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String ordersEventsTopicName;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository,
+                            KafkaTemplate kafkaTemplate,
+                            @Value("${orders.events.topic.name}") String ordersEventsTopicName) {
         this.orderRepository = orderRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.ordersEventsTopicName = ordersEventsTopicName;
     }
 
     @Override
@@ -22,6 +31,15 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
         orderRepository.save(entity);
+
+        OrderCreatedEvent orderCreatedEvent = new OrderCreatedEvent(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity()
+        );
+
+        kafkaTemplate.send(this.ordersEventsTopicName, orderCreatedEvent);
 
         return new Order(
                 entity.getId(),
