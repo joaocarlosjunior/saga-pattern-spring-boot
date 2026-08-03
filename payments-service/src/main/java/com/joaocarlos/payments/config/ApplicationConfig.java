b8b9b1ac00466d18aeb79c365567b6b2ffef62ab
João Carlos Júnior <joaocarlos.cic1@gmail.com>
2026-08-03 10:27:05 -0300

    feat: altera RestTemplate para RestClient

--- payments-service/src/main/java/com/joaocarlos/payments/config/ApplicationConfig.java
+++ payments-service/src/main/java/com/joaocarlos/payments/config/ApplicationConfig.java
@@ -1,15 +1,14 @@
 package com.joaocarlos.payments.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public RestTemplate ccpRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+    public RestClient ccpRestTemplate() {
+        return RestClient.create();
     }
 }
--- payments-service/src/main/java/com/joaocarlos/payments/service/CreditCardProcessorRemoteServiceImpl.java
+++ payments-service/src/main/java/com/joaocarlos/payments/service/CreditCardProcessorRemoteServiceImpl.java
@@ -2,34 +2,42 @@ package com.joaocarlos.payments.service;
 
 import com.joaocarlos.core.dto.CreditCardProcessRequest;
 import com.joaocarlos.core.exceptions.CreditCardProcessorUnavailableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 @Service
 public class CreditCardProcessorRemoteServiceImpl implements CreditCardProcessorRemoteService {
-    private final RestTemplate restTemplate;
     private final String ccpRemoteServiceUrl;
-
+    private final RestClient restClient;
+    private static final Logger logger = LoggerFactory.getLogger(CreditCardProcessorRemoteServiceImpl.class);
 
     public CreditCardProcessorRemoteServiceImpl(
-            RestTemplate restTemplate,
-            @Value("${remote.ccp.url}") String ccpRemoteServiceUrl
-    ) {
-        this.restTemplate = restTemplate;
+            @Value("${remote.ccp.url}") String ccpRemoteServiceUrl,
+            RestClient restClient) {
         this.ccpRemoteServiceUrl = ccpRemoteServiceUrl;
+        this.restClient = restClient;
     }
-
 
     @Override
     public void process(BigInteger cardNumber, BigDecimal paymentAmount) {
         try {
+            logger.info("Recebida ordem para processamento de pagamento");
             var request = new CreditCardProcessRequest(cardNumber, paymentAmount);
-            restTemplate.postForObject(ccpRemoteServiceUrl + "/ccp/process", request, CreditCardProcessRequest.class);
+            restClient.post()
+                    .uri(ccpRemoteServiceUrl + "/ccp/process")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(request)
+                    .retrieve()
+                    .toEntity(CreditCardProcessRequest.class);
+            logger.info("Finalizado ordem para processamento de pagamento");
         } catch (ResourceAccessException e) {
             throw new CreditCardProcessorUnavailableException(e);
         }
